1bae26cf4f30daacb7c9fed5c60acb6981c750b2
Caleb Lam <>
2021-05-11 00:33:03 +0000

    Fixed "development server" warnings

--- keep_alive.py
+++ keep_alive.py
@@ -2,15 +2,16 @@ from flask import Flask
 from waitress import serve
 from threading import Thread
 
-app = Flask('')
+app = Flask("AgendaBot")
 
 @app.route('/')
 def home():
     return "AgendaBot is now online!"
 
 def run():
-  serve(app, host='0.0.0.0', port=8080)
+	serve(app, host='0.0.0.0', port=8080, url_scheme='https')
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+	t = Thread(target=run)
+	t.start()
+	print("Web server initialized!")
